4612b77a70fb3b9cf355984a00ecb50bbc9be3cd
vishwajeetsri <vs0397297@gmail.com>
2025-08-18 20:42:22 +0530

    Exploring GET POST DELETE

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,25 @@ const express = require("express")
 
 const app = express()
 
+app.get("/users",(req,res) => {
+    res.send({firstname : "vishu", lastname : "srivastava" })
+});
+
+app.post("/users",(req,res) => {
+    res.send("Data successfully saved to data base")
+});
+
+app.delete("/users",(req,res) => {
+    res.send("Data deleted")
+});
 
 app.use("/test",(req,res) => {
-    res.send("Hello response from the server test")
+    res.send("test response from the server test")
 });
 
-app.use("/",(req,res) => {
-    res.send("Hello response from the server Home")
-});
+// app.use("/",(req,res) => {
+//     res.send("/ response from the server Home")
+// });
 
 app.listen(3000, () => {
     console.log("Hello from the server5.....")
