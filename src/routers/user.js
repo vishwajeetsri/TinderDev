3a430ccd2da30acbe1a2f6824e29d49f8539a311
vishwajeetsri <vs0397297@gmail.com>
2025-10-10 19:37:26 +0530

    Feed Api and Pagination

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
 const USER_SAFE_DATA = "firstName lastName skills age gender about photoUrl";
+const Users = require("../models/user")
 
 userRouter.get("/user/request/received", userAuth, async (req, res) => {
   try {
@@ -36,17 +37,50 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       .populate("toUserId", USER_SAFE_DATA);
 
     const data = connectionRequests.map((row) => {
-      if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
-        return row.toUserId
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
       }
       return row.fromUserId;
-    } );
+    });
 
     res.json({ data });
   } catch (err) {
     res.status(400).send({
       message: err.message,
     });
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await Users.find({
+      $and: [
+        {_id: { $nin: Array.from(hideUsersFromFeed) }},
+        { _id: { $ne: loggedInUser._id  } }
+      ],
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit)
+
+    res.send(users)
+
+  } catch (err) {
+    res.status(400).json({ messsage: err.message });
   }
 });
 module.exports = userRouter;
